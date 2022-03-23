10122b375aa98f93084439def5ee1d6818aa2820
petrykinaa <base151@rambler.ru>
2022-03-23 14:35:12 +0300

    added pagination, added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,22 @@
 $(function(){
+
+    $('.slider__wrapper').slick({
+
+      arrows: false,
+      dots: true,
+      fade: true,
+      appendDots: $('.dots')
+
+    });
+
+    $('.slider__wrapper').on('wheel', (function (e) {
+      e.preventDefault();
+
+      if (e.originalEvent.deltaY < 0) {
+        $(this).slick('slickNext');
+      } else {
+        $(this).slick('slickPrev');
+      }
+    }));
  
 });
